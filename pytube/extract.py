6e1d91b1a8f6136800a1951e38b7133e3ae98b08
Sourcery AI <>
2020-08-10 23:04:34 +0000

    'Refactored by Sourcery'

--- pytube/extract.py
+++ pytube/extract.py
@@ -273,8 +273,6 @@ def apply_descrambler(stream_data: Dict, key: str) -> None:
     {'foo': [{'bar': '1', 'var': 'test'}, {'em': '5', 't': 'url encoded'}]}
 
     """
-    otf_type = "FORMAT_STREAM_TYPE_OTF"
-
     if key == "url_encoded_fmt_stream_map" and not stream_data.get(
         "url_encoded_fmt_stream_map"
     ):
@@ -284,6 +282,8 @@ def apply_descrambler(stream_data: Dict, key: str) -> None:
                 "adaptiveFormats"
             ]
         )
+        otf_type = "FORMAT_STREAM_TYPE_OTF"
+
         try:
             stream_data[key] = [
                 {
